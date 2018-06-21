ffc50e7f8622c5c7d47fe53329f4e9c83f4ba5be
Juliano J. S. Tavares <julianojstavares@outlook.com>
2018-06-21 18:08:03 -0300

    updating files

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
+var i=1;
+function slideshow() {
+	for (var start = 1; start < 100; start++){
+		setTimeout(function () {
+			document.getElementById("banner").style.backgroundImage = "url(img/b"+i+".png)";
+			if(i>=5){
+				i=0;
+			}
+			i++; 
+			console.log(i);  
+		}, 1000 * start);	
+	}
+}
 window.onload = function() {
   //slideshow
-  //alert("Seja bem-vindo!"); 
+  slideshow();
 };
